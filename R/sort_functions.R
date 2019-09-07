91e011f054aea1853262a30a2f17bee03655a1fd
Maciej Bak <wsciekly.maciek@gmail.com>
2019-09-07 15:46:00 +0200

    added bozosort and its test

--- R/sort_functions.R
+++ R/sort_functions.R
@@ -1,3 +1,5 @@
+# ===================================================================
+
 #' @title Bogosort function
 #'
 #' @description Sorting a numeric vector with bogosort algorithm.
@@ -11,13 +13,42 @@
 bogosort = function(a) {
   len = length(a)
   while (is.unsorted(a)) {
+    # pseudorandom permutation:
     indices = sample(len)
     a = a[indices]
   }
   return(a)
 }
 
-# bozosort
+# ===================================================================
+
+#' @title Bozosort function
+#'
+#' @description Sorting a numeric vector with bozosort algorithm.
+#' @param a Numeric vector to be sorted
+#' @keywords bozosort
+#' @return Numeric vector
+#' @export
+#' @examples
+#' bozosort(c(1,2,4,3))
+
+bozosort = function(a) {
+  len = length(a)
+  while (is.unsorted(a)) {
+    indices = sample(x=len, size=2)
+    # swap two elements:
+    swap = a[indices[1]]
+    a[indices[1]] = a[indices[2]]
+    a[indices[2]] = swap
+  }
+  return(a)
+}
+
+# ===================================================================
+
+
+
+
 
 # gorosort
 
